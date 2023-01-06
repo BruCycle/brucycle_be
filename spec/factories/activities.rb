79467366391be133497b83c1933701f118d66614
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-06 09:58:48 -0700

    Fix latitude to ensure only USA is included

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     calories_burned { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
     gas_money_saved { Faker::Number.decimal(l_digits: 1, r_digits: 2) }
     beers_banked { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
-    latitude { Faker::Number.between(from: 35, to: 45)}
+    latitude { Faker::Number.between(from: 35, to: 40)}
     longitude { Faker::Number.between(from: -120, to: -80)}
     strava_uid { "#{Faker::Number.number(digits: 10)}" }
     user
