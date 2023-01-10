2ba7a48f5b331a7f48c3d7a64dc1123d39a03fa0
Krisetn Nestler <kristen.nestler@gmail.com>
2023-01-10 15:09:02 -0500

    feat: gift brus methods and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
     self.update(brubank: self.brubank - 1)
     self.update(beers_drunk: self.beers_drunk + 1)
   end
+
+  def remove_beer_from_brubank
+    self.update(brubank: self.brubank - 1)
+  end
+
+  def add_beer_to_brubank
+    self.update(brubank: self.brubank + 1)
+  end
+
+  def gift_beer(recipient)
+    if self.brubank >= 1
+      self.remove_beer_from_brubank & recipient.add_beer_to_brubank
+    else 
+      "Not enough brü's, bro"
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,5 +14,39 @@ RSpec.describe User do
         expect(user.beers_drunk).to eq(34)
        end
     end
+
+    describe '#remove_beer_from_brubank' do
+      it 'deletes a beer from the users brubank' do
+        user = create(:user, brubank: 10, beers_drunk: 33)
+        user.remove_beer_from_brubank
+        expect(user.brubank).to eq(9)
+      end
+    end
+
+    describe '#add_beer_to_brubank' do
+      it 'adds a beer to a users brubank' do
+        user = create(:user, brubank: 10, beers_drunk: 33)
+        user.add_beer_to_brubank
+        expect(user.brubank).to eq(11)
+      end
+    end
+
+    describe '#gift_beer' do
+      it 'deletes a beer from the users brubank and adds a beer to a recipeints brubank' do
+        user = create(:user, brubank: 10)
+        user2 = create(:user, brubank: 1)
+        user.gift_beer(user2)
+        expect(user.brubank).to eq(9)
+        expect(user2.brubank).to eq(2)
+      end
+
+      it 'returns error if user does not enough beers in brubank to gift' do
+        user = create(:user, brubank: 0)
+        user2 = create(:user, brubank: 2)
+        expect(user.gift_beer(user2)).to eq("Not enough brü's, bro")
+        expect(user.brubank).to eq(0)
+        expect(user2.brubank).to eq(2)
+      end
+    end
   end
 end
